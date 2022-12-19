f5d14593879b08db477c0046e0de0433db01aad9
Frank Pigeon Jr <frank.pigeonjr@gmail.com>
2022-12-18 20:14:32 -0600

    renames comp

--- frontend/src/components/SlickSlider.jsx
+++ frontend/src/components/SlickSlider.jsx
@@ -3,7 +3,7 @@ import Slider from "react-slick"
 import "slick-carousel/slick/slick.css"
 import "slick-carousel/slick/slick-theme.css"
 
-export default function SimpleSlider({ images, strapiUrl }) {
+export default function SlickSlider({ images, strapiUrl }) {
   var settings = {
     infinite: true,
     speed: 400,
