218430728d28e9694b6c51c05136c853a40ab77b
Frank Pigeon Jr <frank.pigeonjr@gmail.com>
2022-12-28 11:28:01 -0600

    adds responsive sized for mobile

--- frontend/src/components/SlickSlider.jsx
+++ frontend/src/components/SlickSlider.jsx
@@ -6,10 +6,28 @@ import "slick-carousel/slick/slick-theme.css"
 export default function SlickSlider({ images, strapiUrl }) {
   var settings = {
     infinite: true,
+    dots: false,
     speed: 400,
-    slidesToShow: 2,
+    slidesToShow: 3,
     slidesToScroll: 1,
-    autoplay: true
+    arrows: false,
+    autoplay: true,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
   }
 
   return (
